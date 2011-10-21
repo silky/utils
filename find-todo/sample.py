064a6016927430ca5db04050d7f3366d3da6521f
Noon Silk <superhappyfun@gmail.com>
2011-10-21 21:57:46 +1100

    Added readme and sample.

---
+++ find-todo/sample.py
@@ -0,0 +1,12 @@
+# Find todo will find comments of the form:
+#
+# TODO:
+#   - Implement new feature, that
+#       I feel like describing over several lines.
+#   - And another thing!
+
+#   Ignored!
+import sys
+
+# TODO: import io at some point.
+
