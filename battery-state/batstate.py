ec5b51941f1b1cc54103c5afc31868122bbd1ff9
Noon Silk <superhappyfun@gmail.com>
2012-01-14 21:03:06 +1100

    Battery state checker, for in my zsh.

---
+++ battery-state/batstate.py
@@ -0,0 +1,53 @@
+# coding=UTF-8
+# Based on: http://www.masnun.me/2010/09/01/python-script-to-monitor-laptop-battery-charge-ubuntulinux-mint.html
+#      and: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/#my-right-prompt-battery-capacity
+import commands, math, sys
+
+def display_state ():
+    remaining = float(
+            commands.getoutput('grep "^remaining capacity" /proc/acpi/battery/BAT0/state | awk \'{ print $3 }\'')
+            )
+    full = float(
+            commands.getoutput('grep "^last full capacity" /proc/acpi/battery/BAT0/info | awk \'{ print $4 }\'')
+            )
+    
+    fraction = remaining/full
+
+    full  = "▲"
+    empty = "△"
+
+    green  = '\033[32m'
+    yellow = '\033[33m'
+    red    = '\033[31m'
+    cyan   = '\033[36m'
+    color_reset = '\033[00m'
+
+    total_slots = 3
+
+    tol = 0.05 # Claim we're at 100% if we're this close.
+
+    filled = int(math.floor(total_slots * (fraction + tol)))
+    result = '%s%s' % ((total_slots - filled) * empty, full * filled)
+
+    color_out = red
+
+    if fraction > .15:
+        color_out = cyan
+
+    if fraction > .3:
+        color_out = yellow
+
+    if fraction > .6:
+        color_out = green
+
+    result = result.strip(' ')
+    if fraction != 1:
+        # The percents are required by zsh, so that the colour chars
+        # don't take up physical space.
+        print '%{' + color_out + '%}', result, '%{' + color_reset + '%}'
+    else:
+        # Say nothing.
+        pass
+
+if __name__ == "__main__":
+    display_state()
